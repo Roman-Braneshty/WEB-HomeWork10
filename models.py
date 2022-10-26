a8e5316ec799c30b0eff7fca661e336f8016aeea
Roman-Braneshty <102828727+Roman-Braneshty@users.noreply.github.com>
2022-10-26 17:51:46 +0300

    Initial commit

---
+++ models.py
@@ -0,0 +1,18 @@
+
+from mongoengine import *
+
+
+class Contacts(Document):
+    user_name = StringField(required=True, unique=True)
+    birthday = StringField(max_length=50)
+    email = StringField(max_length=100)
+    address = StringField(max_length=100)
+    phones = ListField(StringField(max_length=200, unique=True))
+
+
+class Notates(Document):
+    _id = SequenceField(required=True)
+    notate = StringField(max_length=50, nullable=False)
+    tag = ListField(StringField(max_length=50))
+
+
